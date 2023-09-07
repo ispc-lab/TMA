06800bf129de85706f6484022bebfc573e95d44e
Haotian Liu <53611983+HTLeoo@users.noreply.github.com>
2023-09-07 09:30:13 +0800

    Create train_split.py

---
+++ train_split.py
@@ -0,0 +1,193 @@
+import sys
+sys.path.append('model')
+import time
+import os
+import random
+from tqdm import tqdm
+import wandb
+import torch
+import numpy as np
+
+
+from datasets.DSEC_split_loader import make_data_loader
+from utils.file_utils import get_logger
+from evaluate import validate_DSEC
+from model.TMA import TMA
+
+
+MAX_FLOW = 400
+VAL_FREQ = 5000
+SUM_FREQ = 100
+
+
+class Loss_Tracker:
+    def __init__(self, wandb):
+        self.running_loss = {}
+        self.total_steps = 0
+        self.wandb = wandb
+
+    def push(self, metrics):
+        self.total_steps += 1
+        
+        for key in metrics:
+            if key not in self.running_loss:
+                self.running_loss[key] = 0.0
+            
+            self.running_loss[key] += metrics[key]
+        
+        if self.total_steps % SUM_FREQ == 0:
+            if self.wandb:
+                wandb.log({'Train-EPE': self.running_loss['epe']/SUM_FREQ}, step=self.total_steps)
+            self.running_loss = {}
+
+class Trainer:
+    def __init__(self, args):
+        self.args = args
+        self.model = TMA(input_bins=15)
+        self.model = self.model.cuda()
+        print('Model Done!')
+        
+        #Loader
+        self.train_loader = make_data_loader('train', args.batch_size, args.num_workers)
+        print('Train loader Done!')
+
+        #Optimizer and scheduler for training
+        self.optimizer = torch.optim.AdamW(
+            self.model.parameters(),
+            lr=args.lr,
+            weight_decay=0.0001
+        )
+        self.scheduler = torch.optim.lr_scheduler.OneCycleLR(
+            self.optimizer, 
+            max_lr=args.lr,
+            total_steps=args.num_steps + 100,
+            pct_start=0.01,
+            cycle_momentum=False,
+            anneal_strategy='linear')
+
+
+        #Logger
+        self.checkpoint_dir = args.checkpoint_dir
+        if not os.path.exists(self.checkpoint_dir):
+            os.mkdir(self.checkpoint_dir)
+        self.writer = get_logger(os.path.join(self.checkpoint_dir, 'train_test.log'))
+        self.tracker = Loss_Tracker(args.wandb)
+        
+        self.best_epe = 100
+        self.best_step = None
+        
+        self.writer.info('====A NEW TRAINING PROCESS====')
+
+    def train(self):
+        self.writer.info(self.args)
+        self.model.train()
+
+        total_steps = 0
+        keep_training = True
+        while keep_training:
+
+            bar = tqdm(enumerate(self.train_loader),total=len(self.train_loader), ncols=60)
+            for index, (voxel1, voxel2, flow_map, valid2D) in bar:
+
+                self.optimizer.zero_grad()
+                flow_preds = self.model(voxel1.cuda(), voxel2.cuda())
+                flow_loss, loss_metrics = sequence_loss(flow_preds, flow_map.cuda(), valid2D.cuda(), self.args.weight, MAX_FLOW)
+
+
+                flow_loss.backward()
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.args.grad_clip)
+                self.optimizer.step()
+                self.scheduler.step()
+                
+                bar.set_description(f'Step: {total_steps}/{self.args.num_steps}')
+                self.tracker.push(loss_metrics)
+
+                total_steps += 1
+                results = {}
+                if total_steps % VAL_FREQ == 0:
+                    results.update(validate_DSEC(self.model))
+                    if self.args.wandb:
+                        wandb.log({'VAL-EPE': results['dsec-epe']}, step=total_steps)
+                    if  results['dsec-epe'] < self.best_epe:
+                        ckpt_path = os.path.join(self.args.checkpoint_dir, 'best.pth')
+                        torch.save(self.model.state_dict(), ckpt_path)
+                        self.best_epe = results['dsec-epe']
+                        self.best_step = total_steps + 1
+                    self.writer.info(f'The best epe during the whole training is: {self.best_epe} at step {self.best_step}')
+                    self.model.train()
+
+
+                if total_steps >= self.args.num_steps:
+                    keep_training = False
+                    break
+            time.sleep(0.03)
+        
+        ckpt_path = os.path.join(self.args.checkpoint_dir, 'checkpoint.pth')
+        torch.save(self.model.state_dict(), ckpt_path)
+        return ckpt_path 
+
+
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+def sequence_loss(flow_preds, flow_gt, valid, gamma=0.8, max_flow=MAX_FLOW):
+    """ Loss function defined over sequence of flow predictions """
+    n_predictions = len(flow_preds)    
+    flow_loss = 0.0
+
+    # exlude invalid pixels and extremely large diplacements
+    mag = torch.sum(flow_gt**2, dim=1).sqrt()#b,h,w
+    valid = (valid >= 0.5) & (mag < max_flow)#b,1,h,w
+
+    for i in range(n_predictions):
+        i_weight = gamma**(n_predictions - i - 1)
+        i_loss = (flow_preds[i] - flow_gt).abs()
+        flow_loss += i_weight * (valid[:, None] * i_loss).mean()
+
+    epe = torch.sum((flow_preds[-1] - flow_gt)**2, dim=1).sqrt()
+    epe = epe.view(-1)[valid.view(-1)]
+
+    metrics = {
+        'epe': epe.mean().item(),
+        '1px': (epe < 1).float().mean().item(),
+        '3px': (epe < 3).float().mean().item(),
+        '5px': (epe < 5).float().mean().item(),
+    }
+
+    return flow_loss, metrics
+
+if __name__=='__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='TMA_train_split')
+    #training setting
+    parser.add_argument('--num_steps', type=int, default=200000)
+    parser.add_argument('--checkpoint_dir', type=str, default='') 
+    parser.add_argument('--lr', type=float, default=2e-4)
+    #datasets setting
+
+    #dataloader setting
+    parser.add_argument('--batch_size', type=int, default=6)
+    parser.add_argument('--num_workers', type=int, default=8)
+
+    #model setting
+
+    parser.add_argument('--grad_clip', type=float, default=1)
+
+    # loss setting
+    parser.add_argument('--weight', type=float, default=0.8)
+
+    # wandb project setting
+    parser.add_argument('--wandb', action='store_true', default=False)
+
+    args = parser.parse_args()
+    set_seed(1)
+    if args.wandb:
+        wandb_name = args.checkpoint_dir.split('/')[-1]
+        wandb.init(name=wandb_name, project='TMA_DSEC_split')
+
+    trainer = Trainer(args)
+    trainer.train()
